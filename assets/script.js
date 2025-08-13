68562d0edb05b775dfeab0e7284016cc88ed1c13
Victor Abiodun Animasahun <123975484+VictorAnimasahun@users.noreply.github.com>
2025-08-13 04:11:15 +0100

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+const overlay = document.querySelector('.overlay');
+
+navbarToggle.addEventListener('click', () => {
+	navbarToggle.classList.toggle('active');
+	navbarMenu.classList.toggle('active');
+	overlay.classList.toggle('active');
+});
+
+overlay.addEventListener('click', () => {
+	// Close everything when clicking the overlay
+	navbarToggle.classList.remove('active');
+	navbarMenu.classList.remove('active');
+	overlay.classList.remove('active');
+});
