805e9a54deec1b22bdbb05ac2b91a1a516057935
Victor A. Animasahun <ashonibarevik@gmail.com>
2025-08-19 09:57:50 +0100

    update

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,23 @@ overlay.addEventListener('click', () => {
 	navbarMenu.classList.remove('active');
 	overlay.classList.remove('active');
 });
+
+let myList = ["I love cats", "I enjoy long walks", "I am a voracious reader"];
+  let count = 0;
+  let spanElement = document.getElementById("changing");
+
+  setInterval(() => {
+    // Add "hide" class to start animation
+    spanElement.classList.add("hide");
+
+    // After animation finishes (0.6s), change text
+    setTimeout(() => {
+      spanElement.textContent = myList[count];
+      spanElement.classList.remove("hide"); // bring it back
+      count++;
+
+      if (count === myList.length) {
+        count = 0;
+      }
+    }, 600); // matches the CSS transition duration
+  }, 3000);
